bcb5de6bd64fd99e2719ceb4c1e720c5e97926ca
venkateshpalagiri <venkateshpalagiri@gmail.com>
2023-12-11 22:59:31 -0500

    regular commit

--- src/com/venkatesh/java/collections/linkedlist/LinkedListMain.java
+++ src/com/venkatesh/java/collections/linkedlist/LinkedListMain.java
@@ -1,5 +1,12 @@
 package com.venkatesh.java.collections.linkedlist;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import com.venkatesh.java.entity.Employee;
+
 public class LinkedListMain {
+	
+	List<Employee> linkedList=new LinkedList();
 
 }
