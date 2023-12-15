6f341b158dcddc3bfa6e35d30a6d67d03eeca379
venkateshpalagiri <venkateshpalagiri@gmail.com>
2023-12-15 15:14:59 -0500

    CollectionMethod interface created

--- src/com/venkatesh/java/collections/arraylist/ArrayListDemo.java
+++ src/com/venkatesh/java/collections/arraylist/ArrayListDemo.java
@@ -3,25 +3,27 @@ package com.venkatesh.java.collections.arraylist;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import com.venkatesh.java.collections.methods.CollectionMethods;
 import com.venkatesh.java.entity.Employee;
 
-public class ArrayListDemo {
+public class ArrayListDemo implements CollectionMethods {
 	
 	ArrayList<Employee> arrayList=new ArrayList();
 	ArrayList<Employee> arrayList1=new ArrayList();
-	
+	@Override
 	public void addEmployee(Employee employee) {
 		arrayList.add(employee);
 		
 		System.out.println(employee.toString());
 	}
+	@Override
 	public void deleteEmployee(long employeeId) {
 		System.out.println(arrayList.toString());
 		arrayList.clear();
 		System.out.println(arrayList.toString());
 	}
-	
-	public void addEmployeeForLoop() {
+	@Override
+	public void addEmployeeWithForLoop() {
 		ArrayList<Integer> arrayList=new ArrayList<Integer>();
 		for (int i=0;i<101;i++) {
 			if(i%5==0) {
@@ -37,7 +39,8 @@ public class ArrayListDemo {
 		System.out.println(arrayList);
 	}
 	//Different ways to add Employee object to ArrayList
-	public void typesOfAddingEmployeeObject() {
+	@Override
+	public void addEmployeeWithObject() {
 		//Type-1
 		Employee employee=new Employee(1, "emp1", "Toronto");
 		Employee employee2=new Employee(2, "emp2", "SCR");
---
+++ src/com/venkatesh/java/collections/methods/CollectionMethods.java
@@ -0,0 +1,11 @@
+package com.venkatesh.java.collections.methods;
+
+import com.venkatesh.java.entity.Employee;
+
+public interface CollectionMethods {
+	public void addEmployee(Employee employee);
+	public void deleteEmployee(long employeeId);
+	public void addEmployeeWithForLoop();
+	public void addEmployeeWithObject();
+
+}
