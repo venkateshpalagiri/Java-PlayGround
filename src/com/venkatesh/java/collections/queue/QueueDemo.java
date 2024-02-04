4a18becd385466cc36315220ed4247ff7fb64c1a
venkateshpalagiri <venkateshpalagiri@gmail.com>
2024-02-03 19:54:48 -0500

    Queue in progress

--- src/com/venkatesh/java/collections/queue/QueueDemo.java
+++ src/com/venkatesh/java/collections/queue/QueueDemo.java
@@ -1,5 +1,32 @@
 package com.venkatesh.java.collections.queue;
 
-public class QueueDemo {
+import com.venkatesh.java.collections.methods.CollectionMethods;
+import com.venkatesh.java.entity.Employee;
+
+public class QueueDemo implements CollectionMethods {
+
+	@Override
+	public void addEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteEmployee(long employeeId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addEmployeeWithForLoop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addEmployeeWithObject() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
