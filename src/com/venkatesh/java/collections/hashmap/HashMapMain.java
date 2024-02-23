af060eb0c20d9008ce5b68859f5bb43fa52f86de
venkateshpalagiri <venkateshpalagiri@gmail.com>
2024-02-23 09:00:12 -0500

    HashMap added

--- src/com/venkatesh/java/collections/hashmap/HashMapMain.java
+++ src/com/venkatesh/java/collections/hashmap/HashMapMain.java
@@ -1,7 +1,11 @@
 package com.venkatesh.java.collections.hashmap;
 
 public class HashMapMain {
+	static String str="Venky";
+	static String str1="Venky";
 	public static void main(String[] args) {
+		System.out.println(str.hashCode());
+		System.out.println(str1.hashCode());
 		
 	}
 
